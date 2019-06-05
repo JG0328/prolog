cb75667f590361a6e93a6e6152b744f9f9d162c7
José Gabriel González <jgg032899@gmail.com>
2019-06-05 11:33:08 -0400

    Trabajando en la tercer tarea

--- t3_20160290.pl
+++ t3_20160290.pl
@@ -1,7 +1,12 @@
 % Punto 1
-notaaletra(X,Y):-.
+notaaletra(X,Y):-X>=90,Y='A',integer(X).
+notaaletra(X,Y):-X>=80,X<90,Y='B',integer(X).
+notaaletra(X,Y):-X>=70,X<80,Y='C',integer(X).
+notaaletra(X,Y):-X>=60,X<70,Y='D',integer(X).
+notaaletra(X,Y):-X<60,X>=0,Y='F',integer(X).
+notaaletra(X,Y):-X<0,Y='No es un numero positivo',integer(X).
 % Punto 2
-sucesor(X,Y):-.
+sucesor(X,Y):-Y is X + 1.
 % Punto 3
 
 % Punto 4
