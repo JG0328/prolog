1fa0796596ffbceba5dffe89d76e4e10d4ef5819
José Gabriel González <jgg032899@gmail.com>
2019-06-08 23:37:01 -0400

    Punto 4, números pares, funcional.

--- t3_20160290.pl
+++ t3_20160290.pl
@@ -19,7 +19,17 @@ sucesor(X,Y):-Y is X + 1.
 % Punto 3
 
 % Punto 4
-
+% Se devuelve una lista con los números pares encontrados en la lista
+% inicial. Utilizo par como regla auxiliar para simplificar la regla
+% pares. Para una lista vacía, pares devuelve otra lista vacía (caso más
+% sencillo). X es el número que se está evaluando, la cabeza de la
+% lista. Y toma el valor de la cola. Z es la lista en la que se agregan
+% los números pares. La variable anónima se utiliza para descartar un
+% número, en caso de que sea impar.
+par(X):-X mod 2 =:= 0.
+pares([],[]).
+pares([X|Y],[X|Z]):-par(X),pares(Y,Z).
+pares([_|Y],Z):-pares(Y,Z).
 % Punto 5
 
 
