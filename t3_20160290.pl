358c8c8ecafeb773b3dfc4ec24381727f035a385
José Gabriel González <jgg032899@gmail.com>
2019-06-08 20:31:29 -0400

    Punto 1 y 2, terminados.

--- t3_20160290.pl
+++ t3_20160290.pl
@@ -1,11 +1,20 @@
 % Punto 1
-notaaletra(X,Y):-X>=90,Y='A',integer(X).
-notaaletra(X,Y):-X>=80,X<90,Y='B',integer(X).
-notaaletra(X,Y):-X>=70,X<80,Y='C',integer(X).
-notaaletra(X,Y):-X>=60,X<70,Y='D',integer(X).
-notaaletra(X,Y):-X<60,X>=0,Y='F',integer(X).
-notaaletra(X,Y):-X<0,Y='No es un numero positivo',integer(X).
+% Dada una calificación entera, se devuelve su equivalente en letra,
+% basado en el sistema de calificaciones.
+% X -> El número que se recibe
+% para evaluar su letra equivalente. Y -> El valor de la letra, luego de
+% evaluar.
+notaaletra(X,Y):-X>=90,Y='A'.
+notaaletra(X,Y):-X>=80,X<90,Y='B'.
+notaaletra(X,Y):-X>=70,X<80,Y='C'.
+notaaletra(X,Y):-X>=60,X<70,Y='D'.
+notaaletra(X,Y):-X<60,X>=0,Y='F'.
+notaaletra(X,Y):-X<0,Y='No es un numero positivo'.
 % Punto 2
+% Se determina el sucesor de un número, que es el que se encuentra
+% inmediatamente después de él.
+% X -> El número que se recibe.
+% Y -> El valor del sucesor de X.
 sucesor(X,Y):-Y is X + 1.
 % Punto 3
 
